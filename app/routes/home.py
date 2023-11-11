aa365ef837b9f9a3f84caeaee81cbf2a8f2b2f47
Hans <hansbenson.dytuan@g.msuiit.edu.ph>
2023-11-11 08:29:58 +0800

    Added login_required

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
+from flask_login import login_required
 
 
 home = Blueprint('home', __name__)
 
 @home.route('/')
+@login_required
 def home_page():
   return "<p>Hello, World!</p>"
