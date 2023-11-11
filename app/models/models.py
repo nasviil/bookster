cde4fa7b7630efa3f45afc8ff05dae9f19006b28
Hans <hansbenson.dytuan@g.msuiit.edu.ph>
2023-11-11 08:30:51 +0800

    Initial configurations for Users database

---
+++ app/models/models.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from flask_login import UserMixin
+
+db = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    password = '',
+    database = 'book_app'
+)
+cursor = db.cursor()
+
+class User(UserMixin):
+    def __init__(self, username=None, email= None, password =None):
+        self.username = username
+        self.email = email
+        self.password = password
+
+    @classmethod
+    def userData(cls):
+        cursor = db.cursor()
+        sql = "SELECT * from users"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+    
+    @classmethod
+    def addUser(self, username, email, password):
+        cursor = db.cursor()
+        sql = f"INSERT INTO users(username, email, password) \
+               VALUES('{username}', '{email}', '{password}')"
+        cursor.execute(sql)
+        db.commit()
+        cursor.close()
+        
+    @classmethod
+    def deleteUser(cls, username):
+        cursor = db.cursor()
+        sql = f"DELETE FROM users WHERE username='{username}'"
+        cursor.execute(sql)
+        db.commit()
+        cursor.close()
+
+    
+    @classmethod
+    def userEmail(self, username):
+        cursor = db.cursor()
+        sql = "SELECT * FROM users WHERE username  = %s"
+        cursor.execute(sql, (username,))
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+    
+    @classmethod
+    def userID(self, id):
+        cursor = db.cursor()
+        sql = "SELECT id FROM users WHERE id  = %s"
+        cursor.execute(sql, (id,))
+        result = cursor.fetchone()
+        cursor.close()
+        return result
+
