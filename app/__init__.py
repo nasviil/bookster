9d441f388bf90d1cc5bfbaeb829343cbbc1e9e0e
Hans <hansbenson.dytuan@g.msuiit.edu.ph>
2023-11-12 19:40:09 +0800

    Modified LoginManager fucntion to fit in the auth.py login route

--- app/__init__.py
+++ app/__init__.py
@@ -21,13 +21,13 @@ def create_app():
 
   from .models.models import User
 
-  login_manager = LoginManager(app)
+  login_manager = LoginManager()
   login_manager.login_view = 'auth.login'
   login_manager.init_app(app)
 
 
   @login_manager.user_loader
   def load_user(username):
-      return User.get(username)
+      return User(username)
 
   return app
