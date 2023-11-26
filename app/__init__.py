a3a434f89995662e298bfbc7560f8c6a7679ab7a
Hans <hansbenson.dytuan@g.msuiit.edu.ph>
2023-11-27 03:07:01 +0800

    Attemps in using cloudinary to save image of ID

--- app/__init__.py
+++ app/__init__.py
@@ -15,16 +15,13 @@ def create_app():
   app = Flask(__name__)
   app.config.from_object(Config)
   app.config['SECRET KEY'] = getenv('SECRET_KEY')
+  app.config['SESSION_TYPE'] = 'filesystem'
 
-  cloudinary.config['CLOUD_NAME'] = getenv('CLOUD_NAME')
-  cloudinary.config['API_KEY'] = getenv('API_KEY')
-  cloudinary.config['API_SECRET'] = getenv('API_SECRET')
-  
   # cloudinary.config(
-  #       CLOUD_NAME=CLOUD_NAME,
-  #       API_KEY=API_KEY,
-  #       API_SECRET=API_SECRET
-  #   )
+  #   CLOUD_NAME=getenv('CLOUD_NAME'),
+  #   API_KEY=getenv('API_KEY'),
+  #   API_SECRET=getenv('API_SECRET')
+  # )
 
   from .routes.home import home
   from .routes.auth import auth
@@ -37,7 +34,6 @@ def create_app():
   login_manager = LoginManager()
   login_manager.login_view = 'auth.login'
   login_manager.init_app(app)
-
 
   @login_manager.user_loader
   def load_user(username):
--- app/config.py
+++ app/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config:
     
     SECRET_KEY = getenv('SECRET_KEY')
-    CLOUD_NAME = getenv("CLOUD_NAME")
-    API_KEY = getenv("API_KEY")
-    API_SECRET = getenv("API_SECRET")
+    # CLOUD_NAME = getenv('CLOUD_NAME')
+    # API_KEY = getenv('API_KEY')
+    # API_SECRET = getenv('API_SECRET')
