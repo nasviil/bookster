e853d4a8700a607a05132ec12048f94e203e4f58
nasviil <mrnasviil@gmail.com>
2023-12-06 12:01:43 +0800

    added rent price and sell price

--- app/models/user_book.py
+++ app/models/user_book.py
@@ -19,17 +19,17 @@ class UserBook:
     
     @classmethod
     def get_book_details(cls, book_id):
-        SELECT_SQL = f"SELECT * FROM {cls.__tablename__} JOIN books ON {cls.__tablename__}.book_id = books.book_id WHERE {cls.__tablename__}.book_id = %s"
+        SELECT_SQL = f"SELECT * FROM {cls.__tablename__} JOIN books ON {cls.__tablename__}.book_id = books.book_id JOIN genre ON books.book_genre = genre.genre_id WHERE {cls.__tablename__}.book_id = %s"
         cur = mysql.connection.cursor(dictionary=True)
         cur.execute(SELECT_SQL, (book_id,))
         book_detail = cur.fetchone()
         return book_detail
     
     @classmethod
-    def add_book(cls, user_id, book_title, book_isbn, book_author, book_genre):
+    def add_book(cls, user_id, book_title, book_isbn, book_author, book_genre, book_sell_price, book_rent_price):
             INSERT_BOOK_SQL = (
-                "INSERT INTO books (book_title, book_isbn, book_author, book_genre, date_added) "
-                "VALUES (%s, %s, %s, %s, %s)"
+                "INSERT INTO books (book_title, book_isbn, book_author, book_genre, date_added, book_sell_price, book_rent_price) "
+                "VALUES (%s, %s, %s, %s, %s, %s, %s)"
             )
             INSERT_INSTANCE_SQL = "INSERT INTO user_book_instances (user_id, book_id) VALUES (%s, %s)"
 
@@ -40,7 +40,7 @@ class UserBook:
                 cur.execute("START TRANSACTION")
 
                 # Insert into books
-                cur.execute(INSERT_BOOK_SQL, (book_title, book_isbn, book_author, book_genre, datetime.now()))
+                cur.execute(INSERT_BOOK_SQL, (book_title, book_isbn, book_author, book_genre, datetime.now(), book_sell_price, book_rent_price))
                 book_id = cur.lastrowid
 
                 # Insert into user_book_instances
@@ -55,3 +55,14 @@ class UserBook:
             finally:
                 # Close the cursor
                 cur.close()
+
+class Genre:
+    __tablename__ = 'genre'
+
+    @classmethod
+    def get_genres(cls):
+        SELECT_SQL = f"SELECT * FROM {cls.__tablename__}"
+        cur = mysql.new_cursor(dictionary=True)
+        cur.execute(SELECT_SQL)
+        genres = cur.fetchall()
+        return genres
